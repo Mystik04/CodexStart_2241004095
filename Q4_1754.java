441a0b6e1a347c4f5174ddcd7457806b0efc267e
Sai premjeet Das <121485126+Mystik04@users.noreply.github.com>
2023-10-17 16:02:38 +0530

    Add files via upload

---
+++ Q4_1754.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q4_1754 {
+	public static void main (String[] args) throws IOException {
+		BufferedReader read=new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(read.readLine());
+        StringBuffer ans=new StringBuffer();
+        String []set;
+        double a,b;
+        for(int i=0;i<t;i++) {
+        	set=read.readLine().split(" ");
+        	a=Double.parseDouble(set[0]);
+        	b=Double.parseDouble(set[1]);
+        	
+        	double ans2=(2*b-a)/3;
+        	if(!(a>2*b || b>2*a) && ans2==(long)ans2) {
+        		ans.append("YES\n");
+        	}
+        	else
+        		ans.append("NO\n");
+        }
+        System.out.println(ans);
+        
+	}
+}
